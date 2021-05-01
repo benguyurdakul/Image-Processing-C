64da113b50232a0aeb22f1dcdd62c6031e71de9a
cisem00 <83174891+cisem00@users.noreply.github.com>
2021-05-01 20:20:32 +0300

    Update pgm.c

--- pgm.c
+++ pgm.c
@@ -32,23 +32,53 @@ PGMInfo pgm_read(const char *filename) {
 
     /* TODO: Dosyayi acin. Eger dosya acilamazsa pgm_info'nun error
      * uyesini PGM_ERROR_READ yapip fonksiyonu sonlandirin. */
+    FILE *fp=fopen(filename,"r");//dosya actik
+    if(fp==NULL){//kontrol
+    	pgm_info.error=1;//error kodunu dosya hatasina esitledik
+    	return pgm_info;
+    }
 
 
     /* TODO: PGM imzasi P2 veya P5 degilse dosyayi kapatin, error'u
      * PGM_ERROR_SIGNATURE yapip fonksiyonu sonlandirin. */
+    if(strcmp(pgm_info.signature,"P2")!=0 || strcmp(pgm_info.signature,"P5")!=0){
+    	pgm_info.error=2;
+    	return pgm_info;
+    }
+    /*if((pgm_info.signature[1]!='2') || (pgm_info.signature[1]!='5') ){//kontrol
+    	pgm_info.error=2;//error kodunu imza hatasina esitledik
+    	return pgm_info;
+    }*/
 
     /* Comment satirini oku. */
+    //fscanf(pgm,"%s",pgm_info.comment);
+    int ch=fgetc(fp);
+    if(ch=='#'){
+    	while(ch!=EOF){
+    		fscanf(ch,"%c",pgm_info.comment);
+
+    	}
+    }
 
     /* TODO: En ve boyu oku */
+    fscanf(pgm,"%d",pgm_info.widht);//en
+    fscanf(pgm,"%d",pgm_info.height);//boy
 
     /* Max piksel degerini oku */
+    fscanf(pgm,"%d",pgm_info.max_pixel_value);
 
     /* TODO: pgm_info.pixels icin malloc() ile yer ayiralim.
      * Bir piksel bellekte 1 bayt yer istiyor, unutmayalim. */
+    pgm_info.pixels=malloc(pgm_info.width*pgm_info.height);
 
 
     /* TODO: malloc() ile yer ayrilamazsa dosyayi kapatin, error'u
      * PGM_ERROR_MALLOC yapip fonksiyonu sonlandirin.*/
+    		if(pgm_info.pixels==NULL){
+    			fclose(pgm);
+    			pgm_info.error=3;
+    			return pgm_info;
+    		}
 
 
     /* 2 farkli dosya bicimi, 2 farkli okuma bicimi.
@@ -60,6 +90,26 @@ PGMInfo pgm_read(const char *filename) {
      * P5 yani binary olanda ise dogrudan fread() ile bir kerede pixels'in icerisine
      * okuma yapiyoruz.
      */
+
+    switch(pgm_info.signature[1]){
+    case'2':
+    	for(i=0;i<pgm_info.width*pgm_info.height;i++){
+    		fgets(line,sizeof(line),pgm);
+
+    		if(line!='\n'){
+    			pgm_into.pixels[i]=atoi(line);
+    			read++;
+    		}
+
+    	}
+
+     break;
+
+    case'5':
+    read=fread(pgm_info.pixels,pgm_info.width*pgm_info.height,1,pgm);
+    break;
+    }
+    fclose(pgm);
 
     /* Eger dogru okuma yapamadiysaniz programiniz assert() sayesinde
      * yarida kesilecek. */
@@ -82,13 +132,33 @@ int pgm_write(const char *filename, PGMInfo pgm_info) {
 
     /* TODO: Dosyayi write kipinde acin. Acma basarisiz olursa fonksiyon
      * 1 dondurerek sonlanmalidir. */
+    pgm=fopen(filename,"w");
+    if(pgm==NULL){
+    	return 1;
+    }
 
     /* TODO: Baslik yapisini fprintf() ile dosyaya yazin */
+    fprintf(pgm,"%s\n",pgm_info,signature);
+    fprintf(pgm,"%s",pgm_info.comment);
+    fprintf(pgm,"%d\n",pgm_info.width);
+    fprintf(pgm,"%d\n",pgm_info.height);
+    fprintf(pgm,"%d\n",pgm_info.max_pixel_value);
+
 
     /* TODO: 2 farkli dosya bicimi, 2 farkli yazma bicimi */
+    switch(pgm_info.signature[1]{
+    case'2':
+    for(i=0;i<pgm_info.width*pgm_info.height-1,i++){
+    	fprintf(pgm,"%d\n",pgm_info.pixels[i])
+    }
+    break;
+    case'5';
+    fwrite(pgm_info.pixels,pgm_info.width*pgm_info.height,1,pgm);
+    break;
 
-    
+    }
     /* Dosyayi kapatalim. */
     fclose(pgm);
     return 0;
 }
+
