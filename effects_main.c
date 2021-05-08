5d74ff15a209cbd9cf13aeb77fd30f518110955f
cisem00 <83174891+cisem00@users.noreply.github.com>
2021-05-08 23:14:17 +0300

    Update effects_main.c

--- effects_main.c
+++ effects_main.c
@@ -1,6 +1,9 @@
-#include <stdio.h>
+
+
+   #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 /* Kendi baslik dosyalarimizi include ediyoruz */
 #include "pgm.h"
@@ -21,13 +24,21 @@ int main(int argc, char *argv[])
      * 2 arguman istemektedir. Bu bilgiye gore asagidaki if()'in icerisini
      * doldurun.
      */
+	if(argc<3){
+		fprintf(stderr," Usage: ./pgm_efekt %s <PGM image 1> <PGM image 2> ... <PGM image N>",argv[0]);
+		return 1;
+	}
 
     int i;
     /* Komut satirindan gelen efekt adi */
     char *effect_name = argv[1];
+    char newfile[60];
+    PGMInfo pgm_info;
 
     /* TODO: Bazi efektlerin ihtiyac duyabilecegi rand() rassal
      * sayi uretecini seed edin. */
+    srand(time(NULL));
+    //int rassal = rand();
 
 
     /* argv[0]:     Programin adi
@@ -44,11 +55,35 @@ int main(int argc, char *argv[])
              * dosyanin adini tutacaktir. */
 
             /* Hangi efekt istendiyse ilgili fonksiyon cagir*/
+    for(i=2;i<argc;i++){
+    	pgm_info=pgm_read(argv[i]);
+    	if(pgm_info.error>0){
+    		pgm_print_error(pgm_info);
+    	}
+    	else{
+    		sprintf(newfile,"%s.%s",argv[i],effect_name);
+
+    		if(strcmp(effect_name,"noise")==0){
+    			effect_random_noise(pgm_info.pixels,pgm_info.width,pgm_info.height);
+    		}
+    		else if(strcmp(effect_name,"invert")==0){
+    			effect_invert(pgm_info.pixels,pgm_info.width,pgm_info.height);
+    		}
+    		else if(strcmp(effect_name,"binarize")==0){
+    			effect_threshold(pgm_info.pixels,pgm_info.width,pgm_info.height,160);
+    		}
+    	}
+    }
 
             /* TODO: Yeni PGM dosyasini olusturun. Eger basarisiz olursa
              * ekrana yazdirin. */
+    pgm_write(newfile,pgm_info);
+    if(pgm_write(newfile,pgm_info)!=0){
+    	fprintf(stderr,"Can't write %s\n",newfile);
+    }
 
             /* TODO: pgm_info'daki pixels dizisini free() etmeliyiz. */
+    free(pgm_info.pixels);
 
     return 0;
 }
