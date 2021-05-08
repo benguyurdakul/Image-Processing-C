9263d992bdd558de82156f89a8a2db341df48e3f
cisem00 <83174891+cisem00@users.noreply.github.com>
2021-05-08 23:13:26 +0300

    Update effects.c

--- effects.c
+++ effects.c
@@ -1,10 +1,11 @@
 #include "effects.h"
+#include <stdlib.h>
 
 void effect_random_noise(unsigned char *pixels, int width, int height) {
     int nr_noisy_pixel = (width * height) / 5; // %5 oraninda noise ekle
     int i;
 
-    for (i = 0; i < nr_noisy_pixel; ++i ) {
+    for (i = 0; i < nr_noisy_pixel-1; ++i ) {
         int which_pixel = (rand() % (width * height));
         pixels[which_pixel] = (rand() % 256);
     }
@@ -14,6 +15,10 @@ void effect_random_noise(unsigned char *pixels, int width, int height) {
  * yeni degeri olarak belirler.
  */
 void effect_invert(unsigned char *pixels, int width, int height) {
+	int i;
+	for(i=0;i<height*width;i++){
+		pixels[i]=256-pixels[i];
+	}
 
 }
 
@@ -21,5 +26,12 @@ void effect_invert(unsigned char *pixels, int width, int height) {
  * uzerinde kalanlari ise beyaz yaparak 2 renkli bir resim olusturur.
  */
 void effect_threshold(unsigned char *pixels, int width, int height, int threshold) {
+	int i;
+	for(i=0;i<height*width-1;i++){
+		if(pixels[i]<threshold)
+			pixels[i]=0;
+		else
+			pixels[i]=255;
+	}
 
 }
