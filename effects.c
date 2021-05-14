bd1cd0ee645f4c1c86cf47807b82b33c4a2bbbe9
dorukbulut <76884139+dorukbulut@users.noreply.github.com>
2021-05-14 21:35:14 +0300

    Cogu bitti!!!!!!!!!!!!!!!
    cogu bitti sadece sort algoritması yeniden konuşulacak.

--- effects.c
+++ effects.c
@@ -1,5 +1,11 @@
 #include "effects.h"
 #include <stdlib.h>
+
+void swap_position(unsigned char* pixels,int index1,int index2){
+    int temp = pixels[index1];
+    pixels[index1] = pixels[index2];
+    pixels[index2] = temp;
+}
 void effect_random_noise(unsigned char *pixels, int width, int height) {
     int nr_noisy_pixel = (width * height) / 5; // %5 oraninda noise ekle
     int i;
@@ -38,4 +44,43 @@ void effect_threshold(unsigned char *pixels, int width, int height, int threshol
         if(pixels[i]>threshold) pixels[i] = 0 ;
     }
     
+}
+
+
+
+
+// Parametre olarak aldigi resmin ayna goruntusunu alir
+void effect_mirror(unsigned char* pixels,int width,int height){
+
+    int i,j;
+    
+
+    for(i=0;i<height;i++){
+
+        for(j=0;j<(width/2);j++){
+            
+            
+            swap_position(pixels,(i*width)+j,((width-1)+(i*width))-j);
+        }
+    }
+}
+
+void effect_turn_90 (unsigned char *pixels, int width, int height) {
+	int i, k, j=0;
+	int size = width*height;
+//orijinal diziyi saklamak icin yeni yer aciyoruz. 
+	unsigned char *copy_pixels = malloc(size); 
+	if (copy_pixels == NULL) exit(1);
+	for (i=0; i<size; i=i+1) {
+		copy_pixels[i] = pixels[i];
+	}
+//orijinal diziyi, saklanmis diziyi kullanarak yeniden sekillendiriyoruz
+	for (i=0; i<width; i=i+1) {
+		for (k=(height-1); k>=0; k=k-1) {
+			pixels[j] = copy_pixels[k*width + i]; 
+			j = j + 1;
+		}
+
+        }
+	free(copy_pixels); //yeri bosaltiyoruz
 }
--- effects.h
+++ effects.h
@@ -6,7 +6,9 @@
 void effect_invert(unsigned char *pixels, int width, int height);
 void effect_threshold(unsigned char *pixels, int width, int height, int threshold);
 void effect_random_noise(unsigned char *pixels, int width, int height);
-void effect_smooth(unsigned char *pixels, int width, int height);
+void swap_position(unsigned char* pixels,int index1,int index2);
+void effect_mirror(unsigned char* pixels,int width,int height);
+void effect_turn_90 (unsigned char *pixels, int width, int height);
 
 #endif
 
