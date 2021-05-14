40cf8426e12d87ddc8a0bea43260a46cb1c6d4b0
benguyurdakul <75784006+benguyurdakul@users.noreply.github.com>
2021-05-14 15:31:29 +0300

    Ekstra efektler de yapıldı. Bu dosya tamam.

--- effects.h
+++ effects.h
@@ -6,6 +6,9 @@
 void effect_invert(unsigned char *pixels, int width, int height);
 void effect_threshold(unsigned char *pixels, int width, int height, int threshold);
 void effect_random_noise(unsigned char *pixels, int width, int height);
+void effect_darker(unsigned char *pixels, int width, int height, int percentage);
+void effect_turn_90(unsigned char *pixels, int width, int height);
+void effect_turn_180(unsigned char *pixels, int width, int height);
 
 #endif
 
