495a956afca9d8b2f59e2223dee96d99c5ffc197
benguyurdakul <75784006+benguyurdakul@users.noreply.github.com>
2021-05-14 15:32:10 +0300

    Ekstra efektler eklendi. Bu dosya tamam.

--- effects_main.c
+++ effects_main.c
@@ -5,7 +5,7 @@
 /* Kendi baslik dosyalarimizi include ediyoruz */
 #include "pgm.h"
 #include "effects.h"
-#include<time.h>
+#include <time.h>
 
 
 /* PROGRAMIN ORNEK KULLANIMI:
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
      * doldurun.
      */
     if(argc < 3) {
-	printf("invalid command!\n Usage: ./pgm_efekt <invert|binarize|noise|smooth> <PGM image 1> <PGM image 2> ... <PGM image N>");
+	printf("invalid command!\n Usage: ./pgm_efekt <invert|binarize|noise|smooth> <PGM image 1> <PGM image 2> ... <PGM image N>\n");
 	exit(1);
     }
 
@@ -42,7 +42,7 @@ int main(int argc, char *argv[])
 
 
     /* argv[0]:     Programin adi
-     * argv[1]:     Efekt cesidi: invert, binarize, noise
+     * argv[1]:     Efekt cesidi: invert, binarize, noise, darker, turn
      * argv[2] ...  argv[argc-1]: Dosya adi veya adlari
      */
 
@@ -53,9 +53,16 @@ int main(int argc, char *argv[])
          * resme efekt uygula ve yeni dosyaya kaydet. */
             /* Yeni bir string olustur. Bu string efekt uygulanmis
              * dosyanin adini tutacaktir. */
+     /* Hangi efekt istendiyse ilgili fonksiyon cagir*/
 
-    PGMInfo retval;  //geridonus degeri
-    char new_doc[100];   //saklanacak string
+            /* TODO: Yeni PGM dosyasini olusturun. Eger basarisiz olursa
+             * ekrana yazdirin. */
+
+    PGMInfo retval;    //geridonus degeri
+    char new_doc[100]; //saklanacak string
+    int user_value;    //kullanicidan alinacak deger
+    int a;             //uyariyi yok etmek -> scanf icin
+
     for (i=2; i<argc; i=i+1) {
 	retval = pgm_read(argv[i]);
         if (retval.error > 0) {
@@ -70,31 +77,53 @@ int main(int argc, char *argv[])
 	if (strcmp(effect_name, "invert") == 0) {
 		effect_invert(retval.pixels, retval.width, retval.height);
         }
+
         else if (strcmp(effect_name, "binarize") == 0) {
 		effect_threshold(retval.pixels, retval.width, retval.height, rand_value);
         }
+
         else if (strcmp(effect_name, "noise") == 0) {
 		effect_random_noise(retval.pixels, retval.width, retval.height);
         }
 	
-        if (pgm_write(new_doc, retval) != 0) {
+	else if (strcmp(effect_name, "darker") == 0) {
+               //koyulastirma yuzdesini kullanicidan alip fonksiyona gonderecegiz
+		printf("\nEnter the percentage: \n"); 
+		a = scanf("%d", &user_value);
+               //uyariyi yok etmek icin a ya atadim, onemsiz
+		effect_darker(retval.pixels, retval.width, retval.height, user_value);
+        } 
+
+	else if (strcmp(effect_name, "turn") == 0) {
+               //iki secenek var, kullanicidan secim yapmasini istiyoruz (90 yada 180)
+		printf("\nselect the degree of rotation (90 or 180) : \n");
+		a = scanf("%d", &user_value);
+	       //uyariyi yok etmek icin a ya atadim, onemsiz
+
+		switch (user_value) {
+		case 90 :
+			effect_turn_90(retval.pixels, retval.width, retval.height);
+//Yeni olusturulan dosyanın width ve height degerleri guncellenmeli!
+//Bu sebeple retval.width ve retval.height degerlerini guncelliyorum
+			int temp = retval.width;   
+			retval.width = retval.height; 
+			retval.height = temp; 
+			break;
+
+		case 180 :
+			effect_turn_180(retval.pixels, retval.width, retval.height);
+			break;
+                }
+	}  
+     
+	if (pgm_write(new_doc, retval) != 0) {
         	printf("ERROR! THE PGM FILE COULD NOT BE CREATED");
 		exit(1);
         }
 
     }
 
-            /* Hangi efekt istendiyse ilgili fonksiyon cagir*/
-
-            /* TODO: Yeni PGM dosyasini olusturun. Eger basarisiz olursa
-             * ekrana yazdirin. */
-  
-
             /* TODO: pgm_info'daki pixels dizisini free() etmeliyiz. */
-    int size = retval.height * retval.width;
-    /*for (i=0; i<size; i=i+1) {
-	free(&retval.pixels[i]);
-    }*/
     free(retval.pixels);
 
 
